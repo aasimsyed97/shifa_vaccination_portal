15ddd3af6bdf25e23cd989024d9946d6cf1607a0
aasimsyed97 <sayedaasim555@gmail.com>
2023-01-20 17:39:55 +0530

    all methods of vaccination center

--- ShifaVaccinationPortal/src/main/java/com/masai/service/VaccinationCenterService.java
+++ ShifaVaccinationPortal/src/main/java/com/masai/service/VaccinationCenterService.java
@@ -2,6 +2,7 @@ package com.masai.service;
 
 import java.util.List;
 
+import com.masai.exception.VaccinationCenterException;
 import com.masai.model.VaccinationCenter;
 import com.masai.repository.VaccinationCenterRepo;
 
@@ -9,5 +10,11 @@ public interface VaccinationCenterService {
  
 	
 	  public List<VaccinationCenter> getAllVaccinationCenter ();
-	
+	  public VaccinationCenter getVaccinCenterById(Integer centerid)throws VaccinationCenterException;
+     public  VaccinationCenter addVaccinationCenter(VaccinationCenter center);
+     public  VaccinationCenter updateVaccinationCenter(VaccinationCenter center)throws VaccinationCenterException; 
+     public VaccinationCenter deleteVaccinationCenter(VaccinationCenter center)throws VaccinationCenterException;
+
 }
+
+
--- ShifaVaccinationPortal/src/main/java/com/masai/service/VaccinationServiceImpl.java
+++ ShifaVaccinationPortal/src/main/java/com/masai/service/VaccinationServiceImpl.java
@@ -1,13 +1,19 @@
 package com.masai.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.masai.exception.VaccinationCenterException;
 import com.masai.model.VaccinationCenter;
 import com.masai.repository.VaccinationCenterRepo;
 
+@Service
 public class VaccinationServiceImpl  implements VaccinationCenterService{
 
+	 @Autowired
 	  private VaccinationCenterRepo vcRepo;
 	
 	@Override
@@ -20,6 +26,61 @@ public class VaccinationServiceImpl  implements VaccinationCenterService{
 		    }
 		 
 		return vclist;
+	}
+
+	@Override
+	public VaccinationCenter getVaccinCenterById(Integer centerid) throws VaccinationCenterException {
+		// TODO Auto-generated method stub 
+		     Optional<VaccinationCenter> opt = vcRepo.findById(centerid); 
+		      if(opt.isPresent()) { 
+		    	  VaccinationCenter vc = opt.get();
+		    	  return vc;
+		      }
+		      else { 
+		    	  throw new VaccinationCenterException("Vaccination center not found");
+		      }
+	}
+
+	@Override
+	public VaccinationCenter addVaccinationCenter(VaccinationCenter center) {
+		// TODO Auto-generated method stub 
+		
+		    VaccinationCenter vc = vcRepo.save(center);
+		    
+		return vc;
+	}
+
+	@Override
+	public VaccinationCenter updateVaccinationCenter(VaccinationCenter center) throws VaccinationCenterException {
+		// TODO Auto-generated method stub 
+		  Optional<VaccinationCenter> opt = vcRepo.findById(center.getCode()); 
+	      if(opt.isPresent()) { 
+	    	  VaccinationCenter vc = vcRepo.save(center);
+	    	     
+	    	    
+	    	  return vc;
+	      }
+	      else { 
+	    	  throw new VaccinationCenterException("Vaccination center not found");
+	      }
+		
+	}
+
+	@Override
+	public VaccinationCenter deleteVaccinationCenter(VaccinationCenter center) throws VaccinationCenterException {
+		// TODO Auto-generated method stub 
+		 Optional<VaccinationCenter> opt = vcRepo.findById(center.getCode()); 
+	      if(opt.isPresent()) { 
+	    	  VaccinationCenter vc = opt.get();
+	    	    vcRepo.delete(vc);
+	    	     
+	    	    
+	    	  return vc;
+	      }
+	      else { 
+	    	  throw new VaccinationCenterException("Vaccination center not found");
+	      }
+		
 	}
 
 }

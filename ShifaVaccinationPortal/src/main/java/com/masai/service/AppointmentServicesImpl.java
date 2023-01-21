a6c782d4b92b440e44834de7ec01689bb4144c63
ashishmonga00 <ashishmonga00@gmail.com>
2023-01-21 18:24:53 +0530

    Done

---
+++ ShifaVaccinationPortal/src/main/java/com/masai/service/AppointmentServicesImpl.java
@@ -0,0 +1,26 @@
+package com.masai.service;
+
+import com.masai.exception.AppointmentException;
+import com.masai.module.Appointment;
+
+public class AppointmentServicesImpl implements AppointmentServices{
+    @Override
+    public Appointment getAppointment(Integer bookingid) throws AppointmentException {
+        return null;
+    }
+
+    @Override
+    public String addAppointment(Appointment app) throws AppointmentException {
+        return null;
+    }
+
+    @Override
+    public String updateAppointment(Appointment app) throws AppointmentException {
+        return null;
+    }
+
+    @Override
+    public String deleteAppointment(Appointment app) throws AppointmentException {
+        return null;
+    }
+}
